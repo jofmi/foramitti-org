5fcae09bed390ec02be28c372760ab54f066d82d
Joël <57440945+jofmi@users.noreply.github.com>
2024-01-15 17:22:17 +0100

    feat: add projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,6 +40,30 @@
 			href: 'https://smartat.coop'
 		}
 	];
+
+	const projects = [
+		{
+			logo: 'images/logos/collectivo.png',
+			href: 'https://collectivo.io',
+			date: '2022 - heute',
+			title: 'Collectivo',
+			subtitle:
+				'Open-Source Plattform für Zusammenarbeit, Partizipation und Datenverwaltung'
+		},
+		{
+			href: 'https://augustin.or.at/mit-bargeld-oder-karte/',
+			title: 'Augustina',
+			date: '2023',
+			subtitle: 'Bargeldloses Bezahlsystem für den Augustin'
+		},
+		{
+			href: 'https://agentpy.readthedocs.io/en/latest/',
+			title: 'AgentPy',
+			date: '2021',
+			subtitle:
+				'Open-Source Paket für die Simulation von Agenten-basierten Modellen'
+		}
+	];
 </script>
 
 <div class="h-full mx-auto flex justify-center items-center">
@@ -63,15 +87,20 @@
 			Berater. Ich unterstütze Organisationen bei der Entwicklung von Prozessen
 			und Strukturen sowie bei der Umsetzung von maßgeschneiderten
 			Software-Lösungen. Mein Schwerpunkt und meine Leidenschaft liegen dabei
-			auf Projekten für gemeinschaftliches, nachhaltiges und
-			bedürfnis-orientiertes Wirtschaften.
+			auf Projekten für <a
+				href="https://se-conference.uni-graz.at/de/politik/oesterreich/#c541464"
+				target="_blank"
+				class="font-bold"
+				>gemeinschaftliches, nachhaltiges und bedürfnis-orientiertes
+				Wirtschaften</a
+			>.
 		</p>
 
 		<div>
 			<a href="mailto:joel@foramitti.org">
 				<button
 					type="button"
-					class="btn variant-filled rounded-md hover:scale-[1.04] duration-100 font-bold"
+					class="btn variant-filled rounded-md hover:scale-[1.04] duration-100 font-bold pf-shadow"
 				>
 					<Icon icon="bi:envelope-at-fill" class="mr-2" /> Kontakt aufnehmen
 				</button>
@@ -82,13 +111,34 @@
 
 		<h2 class="h2">Partner*innen</h2>
 		<div
-			id="partners"
+			id="pf-partners"
 			class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5"
 		>
 			{#each partners as { name, href, logo }}
 				<a {href} target="_blank">
-					<div class="pf-card flex items-center">
+					<div class="pf-card pf-shadow h-36 flex items-center">
 						<img src={logo} alt={name} class="m-auto max-h-[7rem] max-w-48" />
+					</div>
+				</a>
+			{/each}
+		</div>
+
+		<div class="pf-divider"></div>
+
+		<h2 class="h2">Projekte</h2>
+
+		<div
+			id="pf-projects"
+			class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5"
+		>
+			{#each projects as { title, subtitle, date, href }}
+				<a {href} target="_blank">
+					<div class="pf-card pf-shadow h-full">
+						<div class="flex flex-col space-y-2 m-1">
+							<h3 class="h3">{title}</h3>
+							<p class="text-sm italic">{date}</p>
+							<p>{subtitle}</p>
+						</div>
 					</div>
 				</a>
 			{/each}
@@ -98,13 +148,14 @@
 
 		<h2 class="h2">Impressum</h2>
 		<p>
-			<span class="font-bold">Joël Foramitti</span> <br />
-			Email: <a href="mailto:joel@foramitti.org">joel@foramitti.org</a>
+			<span class="">Dr. Joël Foramitti</span> <br />
+
+			<a href="mailto:joel@foramitti.org" class="font-bold"
+				>joel@foramitti.org</a
+			>
 		</p>
 		<p>
-			<a href="https://smartat.coop">
-				<span class="font-bold">smart@work GmbH</span>
-			</a>
+			<span class="">smart@work GmbH</span>
 			<br />Gumpendorferstraße 63b
 			<br />1060 Wien
 		</p>
@@ -120,7 +171,11 @@
 		@apply h-5;
 	}
 
+	.pf-shadow {
+		@apply shadow-[1px_1px_15px_3px] shadow-white/20;
+	}
+
 	.pf-card {
-		@apply bg-white rounded-md h-36 duration-300 transition-all px-4 py-3 hover:scale-[1.03] shadow-[1px_1px_15px_3px] shadow-white/20;
+		@apply bg-white text-black rounded-md duration-300 transition-all px-4 py-3 hover:scale-[1.03];
 	}
 </style>
