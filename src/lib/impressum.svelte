0da1e374d7f4ff8e76194614b7691fd52f6f1b52
Joël <57440945+jofmi@users.noreply.github.com>
2024-02-05 13:51:33 +0100

    Update website

---
+++ src/lib/impressum.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { Modal, getModalStore } from '@skeletonlabs/skeleton';
+	const modalStore = getModalStore();
+</script>
+
+{#if $modalStore[0]}
+	<div class="w-modal-wide bg-white p-10 rounded-md space-y-2">
+		<h2 class="h2">Impressum</h2>
+		<p>
+			Joël Foramitti arbeitet teilweise innerhalb seines
+			Unternehmensgegenstandes angestellt über die smart@work GmbH und führt aus
+			dieser Anstellung heraus die Aufträge auf Werkvertragsbasis aus.
+			Ausführende*r des Auftrages/Werks ist also in diesem Fall Joël Foramitti,
+			Werkvertragspartnerin für den Auftrag ist seine Arbeitgeberin die
+			smart@work GmbH.
+			<br /><br />
+			Die smart@work GmbH hält, persönlich vertreten durch die Geschäftsführerin
+			Angela Vadori, die notwendige Gewerbeberechtigung:
+			<br />
+			Standort der Gewerbeberechtigung: Gumpendorfer Straße 63B, 1060 Wien, Österreich
+			<br />
+			Unternehmensgegenstand: IT-Dienstleistungen & wissenschaftliche Arbeit
+			<br />
+			Kontaktdaten der Halterin der Gewerbeberechtigung: smart@work GmbH, Geschäftsführerin
+			Angela Vadori, office@smartat.coop, +43 1 997 44 66
+			<br />
+			<br />UID Nr.: ATU75116919 FN 525600 t
+			<br />Firmenbuchgericht: Handelsgericht Wien
+			<br />Handelsrechtliche Geschäftsführung: Angela Vadori
+			<br />Mitgliedschaften bei der Wirtschaftskammerorganisation: WKO
+			<br />Anwendbare Rechtsvorschriften und Zugang dazu: Gewerbeordnung
+			<br />Aufsichtsbehörde/Gewerbebehörde: Gewerbebehörde: Magistratisches
+			Bezirksamt, 7., Hermanngasse 24-26
+			<br />Angaben zur Online-Streitbeilegung: Verbraucher*innen haben die
+			Möglichkeit, Beschwerden an die Online-Streitbeilegungsplattform der EU zu
+			richten: http://ec.europa.eu/odr.
+		</p>
+	</div>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,49 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
+	import { initializeStores } from '@skeletonlabs/skeleton';
+	import { Modal, getModalStore } from '@skeletonlabs/skeleton';
+	import type {
+		ModalSettings,
+		ModalComponent,
+		ModalStore
+	} from '@skeletonlabs/skeleton';
+
+	import Impressum from '$lib/impressum.svelte';
+	import Privacy from '$lib/privacy.svelte';
+
+	const modalRegistry: Record<string, ModalComponent> = {
+		Impressum: { ref: Impressum },
+		Privacy: { ref: Privacy }
+	};
+	initializeStores();
+	const modalStore = getModalStore();
+	const impressum: ModalSettings = {
+		type: 'component',
+		component: 'Impressum'
+	};
+	const privacy: ModalSettings = {
+		type: 'component',
+		component: 'Privacy'
+	};
+
 	const icons = [
 		{
+			name: 'Email',
+			label: 'joel@foramitti.org',
+			href: 'mailto:joel@foramitti.org',
+			icon: 'bi:envelope-at-fill'
+		},
+		{
 			name: 'GitHub',
+			label: 'github.com/jofmi',
 			href: 'https://github.com/jofmi',
 			icon: 'bi:github'
 		},
 		{
 			name: 'LinkedIn',
+			label: 'linkedin.com/in/joelforamitti/',
 			href: 'https://www.linkedin.com/in/joelforamitti/',
 			icon: 'bi:linkedin'
-		},
-		{
-			name: 'Email',
-			href: 'mailto:joel@foramitti.org',
-			icon: 'bi:envelope-at-fill'
 		}
 	];
 
@@ -47,135 +76,194 @@
 			href: 'https://collectivo.io',
 			date: '2022 - heute',
 			title: 'Collectivo',
-			subtitle:
-				'Open-Source Plattform für Zusammenarbeit, Partizipation und Datenverwaltung'
+			subtitle: 'Open-Source Plattform für Partizipation und Datenverwaltung'
 		},
 		{
 			href: 'https://augustin.or.at/mit-bargeld-oder-karte/',
 			title: 'Augustina',
 			date: '2023',
-			subtitle: 'Bargeldloses Bezahlsystem für den Augustin'
+			subtitle:
+				'Open-Source System für bargeldloses Bezahlen von Straßenzeitungen'
 		},
 		{
 			href: 'https://agentpy.readthedocs.io/en/latest/',
 			title: 'AgentPy',
 			date: '2021',
 			subtitle:
-				'Open-Source Paket für die Simulation von Agenten-basierten Modellen'
+				'Open-Source Paket für die Simulation von agenten-basierten Modellen'
 		}
 	];
 </script>
 
+<Modal components={modalRegistry} />
+
 <div class="h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5 max-w-3xl w-full m-10 md:m-24 lg:mt-48">
-		<div>
-			<h1 class="h1 mb-2">Joël Foramitti</h1>
-			<p class="text-xl">Wissen und Werkzeuge für kooperatives Wirtschaften</p>
+	<div class="space-y-20 max-w-3xl w-full m-10 md:m-24 lg:mt-48">
+		<div id="pf-title" class="flex flex-wrap gap-10 items-center align-middle">
+			<!-- <div class="drop-shadow-md">
+				<img
+					src="images/joel.jpg"
+					alt="Joël Foramitti"
+					class="rounded-full w-40 overflow-hidden"
+				/>
+			</div> -->
+			<div>
+				<h1 class="h1 mb-2">Joël Foramitti</h1>
+				<p class="text-2xl italic mb-4">
+					Wissen und Werkzeuge für kooperatives Wirtschaften
+				</p>
+				<div class="flex flex-wrap justify-left text-3xl space-x-4">
+					{#each icons as { name, icon, href }}
+						<a {href} target="_blank"
+							><Icon {icon} class="hover:scale-[1.04] duration-100" /></a
+						>
+					{/each}
+				</div>
+			</div>
 		</div>
-		<div class="flex flex-wrap justify-left text-3xl space-x-4">
-			{#each icons as { name, icon, href }}
-				<a {href} target="_blank"
-					><Icon {icon} class="hover:scale-[1.04] duration-100" /></a
-				>
-			{/each}
+
+		<div id="pf-intro" class="space-y-4">
+			<p class="text-lg">
+				Hallo! Ich bin selbstständiger Software-Entwickler und promovierter
+				Wirtschaftsforscher. Ich unterstütze Organisationen bei der Entwicklung
+				von Prozessen und Strukturen für Partizipation und Selbstorganisation -
+				sowie bei der Umsetzung von maßgeschneiderten Software-Lösungen. Mein
+				Schwerpunkt und meine Leidenschaft liegen dabei auf Projekten für <a
+					href="https://se-conference.uni-graz.at/de/politik/oesterreich/#c541464"
+					target="_blank"
+					class="font-bold italic"
+					>gemeinschaftliches, ökologisches und bedürfnis-orientiertes
+					Wirtschaften</a
+				>.
+			</p>
+
+			<!-- <div class="">
+				<a href="mailto:joel@foramitti.org">
+					<button type="button" class="btn font-bold pf-box">
+						<Icon icon="bi:envelope-at-fill" class="mr-2" /> Kontakt aufnehmen
+					</button>
+				</a>
+			</div> -->
 		</div>
 
-		<div class="h-5"></div>
+		<div id="pf-services" class="space-y-6">
+			<h2 class="h2">Schwerpunkte</h2>
 
-		<p>
-			Hallo! Ich bin promovierter Wirtschaftsforscher, Software-Entwickler, und
-			Berater. Ich unterstütze Organisationen bei der Entwicklung von Prozessen
-			und Strukturen sowie bei der Umsetzung von maßgeschneiderten
-			Software-Lösungen. Mein Schwerpunkt und meine Leidenschaft liegen dabei
-			auf Projekten für <a
-				href="https://se-conference.uni-graz.at/de/politik/oesterreich/#c541464"
-				target="_blank"
-				class="font-bold"
-				>gemeinschaftliches, nachhaltiges und bedürfnis-orientiertes
-				Wirtschaften</a
-			>.
-		</p>
+			<div class="grid grid-cols-1 sm:grid-cols-2 gap-5">
+				<div class="pf-box w-full px-6 py-5 space-y-2">
+					<h3 class="h3">Software-Entwicklung</h3>
+					<p class="text-md">
+						Ich biete maßgeschneiderte Lösungen für digitale Plattformen und
+						Datenbanksysteme, basierend auf <a
+							href="#projects"
+							class="link-on-white">Open-Source Technologien</a
+						>.
+					</p>
+					<p class="text-md">
+						Dabei begleite ich den gesamten Entwicklungsprozess von der
+						Konzeption bis zur Umsetzung und langfristigen Wartung.
+					</p>
+				</div>
 
+				<div class="pf-box w-full px-6 py-5 space-y-2">
+					<h3 class="h3">Forschung & Beratung</h3>
+					<p>
+						Ich unterstütze Organisationen bei der Entwicklung von Prozessen und
+						Strukturen zu Partizipation und Selbstorganisation.
+					</p>
+					<p>
+						Dabei verbinde ich wissenschaftliche Methoden mit praktischer
+						Erfahrungen aus <a href="#partners" class="link-on-white"
+							>kooperativen Wirtschaftsprojekten</a
+						>.
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<div id="pf-services" class="space-y-6">
+			<h2 class="h2">Partner*innen</h2>
+			<div
+				id="partners"
+				class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5"
+			>
+				{#each partners as { name, href, logo }}
+					<a {href} target="_blank">
+						<div class="pf-box h-36 flex items-center">
+							<img src={logo} alt={name} class="m-auto max-h-[7rem] max-w-48" />
+						</div>
+					</a>
+				{/each}
+			</div>
+		</div>
+
+		<div id="projects" class="space-y-6">
+			<h2 class="h2">Projekte</h2>
+
+			<div
+				id="projects"
+				class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5"
+			>
+				{#each projects as { title, subtitle, date, href }}
+					<a {href} target="_blank">
+						<div class="pf-box h-full px-6 py-5 space-y-2">
+							<div class="flex flex-col space-y-2 m-1">
+								<h3 class="h3">{title}</h3>
+								<p class="text-sm italic">{date}</p>
+								<p>{subtitle}</p>
+							</div>
+						</div>
+					</a>
+				{/each}
+			</div>
+		</div>
 		<div>
 			<a href="mailto:joel@foramitti.org">
 				<button
 					type="button"
-					class="btn variant-filled rounded-md hover:scale-[1.04] duration-100 font-bold pf-shadow"
+					class="btn font-bold text-2xl rounded-md bg-primary-900 drop-shadow-md w-full px-6 py-5 text-white center"
 				>
-					<Icon icon="bi:envelope-at-fill" class="mr-2" /> Kontakt aufnehmen
+					<Icon icon="bi:envelope-at-fill" class="mr-2" /> Jetzt Kontakt aufnehmen
 				</button>
 			</a>
 		</div>
 
-		<div class="pf-divider"></div>
-
-		<h2 class="h2">Partner*innen</h2>
-		<div
-			id="pf-partners"
-			class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5"
-		>
-			{#each partners as { name, href, logo }}
-				<a {href} target="_blank">
-					<div class="pf-card pf-shadow h-36 flex items-center">
-						<img src={logo} alt={name} class="m-auto max-h-[7rem] max-w-48" />
-					</div>
-				</a>
-			{/each}
+		<div class="pf-box px-6 py-5 text-lg flex flex-wrap items-end">
+			<div>
+				<span class="font-semibold">Joël Foramitti</span>
+				{#each icons as { label, icon, href }}
+					<a {href} target="_blank" class="flex flex-row items-center gap-2">
+						<Icon {icon} class="" />
+						{label}
+					</a>
+				{/each}
+			</div>
+			<div class="grow"></div>
+			<div class="flex flex-col">
+				<button
+					class="link-on-white text-right"
+					on:click={() => modalStore.trigger(impressum)}
+				>
+					Impressum
+				</button>
+				<button
+					class="link-on-white text-right"
+					on:click={() => modalStore.trigger(privacy)}
+				>
+					Datenschutz
+				</button>
+			</div>
 		</div>
-
-		<div class="pf-divider"></div>
-
-		<h2 class="h2">Projekte</h2>
-
-		<div
-			id="pf-projects"
-			class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5"
-		>
-			{#each projects as { title, subtitle, date, href }}
-				<a {href} target="_blank">
-					<div class="pf-card pf-shadow h-full">
-						<div class="flex flex-col space-y-2 m-1">
-							<h3 class="h3">{title}</h3>
-							<p class="text-sm italic">{date}</p>
-							<p>{subtitle}</p>
-						</div>
-					</div>
-				</a>
-			{/each}
-		</div>
-
-		<div class="pf-divider"></div>
-
-		<h2 class="h2">Impressum</h2>
-		<p>
-			<span class="">Dr. Joël Foramitti</span> <br />
-
-			<a href="mailto:joel@foramitti.org" class="font-bold"
-				>joel@foramitti.org</a
-			>
-		</p>
-		<p>
-			<span class="">smart@work GmbH</span>
-			<br />Gumpendorferstraße 63b
-			<br />1060 Wien
-		</p>
-		<p>
-			UID Nr.: ATU75116919
-			<br />FN 525600 t
-		</p>
 	</div>
 </div>
 
 <style lang="postcss">
-	.pf-divider {
-		@apply h-5;
+	.pf-box {
+		@apply bg-white/60 hover:bg-white duration-150 drop-shadow-sm rounded-md text-black;
 	}
 
-	.pf-shadow {
-		@apply shadow-[1px_1px_15px_3px] shadow-white/20;
-	}
-
-	.pf-card {
-		@apply bg-white text-black rounded-md duration-300 transition-all px-4 py-3 hover:scale-[1.03];
+	.link-on-white {
+		@apply no-underline font-semibold text-primary-900;
+		color: --theme-font-color-base;
 	}
 </style>
