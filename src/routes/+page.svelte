52868b45ef5dd5127e6f807e3f08c6938f7f0947
Joël <57440945+jofmi@users.noreply.github.com>
2024-09-16 10:27:33 +0200

    change to shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,11 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
-	import { initializeStores } from '@skeletonlabs/skeleton';
-	import { Modal, getModalStore } from '@skeletonlabs/skeleton';
 	import type { Project } from '$lib/types';
-	import Impressum from '$lib/impressum.svelte';
-	import Privacy from '$lib/privacy.svelte';
-	import projectModal from '$lib/projectModal.svelte';
+	// import Impressum from '$lib/impressum.svelte';
+	// import Privacy from '$lib/privacy.svelte';
+	// import projectModal from '$lib/projectModal.svelte';
 
-	initializeStores();
-	const modalStore = getModalStore();
-
-	function openProjectModal(project: Project) {
-		modalStore.trigger({
-			type: 'component',
-			component: {
-				ref: projectModal,
-				props: { project: project }
-			}
-		});
-	}
+	function openProjectModal(project: Project) {}
 
 	const icons = [
 		{
@@ -79,8 +66,7 @@
 			date: '2022-2024',
 			title: 'Mein MILA',
 			tags: ['Open-Source'],
-			summary:
-				'Mitgliederplattform und Datenbanksystem für den MILA Mitmach-Supermarkt',
+			summary: 'Mitgliederplattform und Datenbanksystem für den MILA Mitmach-Supermarkt',
 			description: `Die MILA Mitgliederplattform "Mein MILA" ist ein partizipatives Tool für den [MILA-Mitmach-Supermarkt e. G.](https://mila.wien). Über die Plattform können Nutzer\*innen einen Beitrittsantrag stellen, sich für Arbeitsschichten und Urlaube eintragen sowie ihre persönlichen Daten aktualisieren.
 
 Das Backend der Plattform ist ein umfassendes Datenbanksystem, das zur Verwaltung von Mitgliederdaten, Rechnungen, Arbeitsschichten und automatischem E-Mail-Versand dient.
@@ -100,8 +86,7 @@ Dieses Projekt wurde vom [AK Digifond](https://wien.arbeiterkammer.at/digifonds)
 		{
 			date: '2023',
 			title: 'Augustina',
-			summary:
-				'Bargeldloses Bezahlsystem und Verwaltungsoberfläche für Straßenzeitungen',
+			summary: 'Bargeldloses Bezahlsystem und Verwaltungsoberfläche für Straßenzeitungen',
 			tags: ['Open-Source'],
 			description: `Das Bezahlsystem Augustina wurde 2023 für die Straßenzeitung Augustin entwickelt und ist seitdem aktiv im Einsatz. Es ermöglicht bargeldlose Zahlungen auf der Straße und dient gleichzeitig als Online-Shop und Verwaltungssystem.
 
@@ -116,8 +101,7 @@ Dieses Projekt wurde vom [AK Digifond](https://wien.arbeiterkammer.at/digifonds)
 			repositoryUrl: 'https://github.com/jofmi/agentpy',
 			title: 'AgentPy',
 			date: '2021',
-			summary:
-				'Wissenschaftliches Software-Paket für agenten-basierte Simulationsmodelle',
+			summary: 'Wissenschaftliches Software-Paket für agenten-basierte Simulationsmodelle',
 			description: `AgentPy ist ein Open-Source-Paket für die Entwicklung und Analyse agenten-basierter Modelle in Python. Das Framework integriert die Aufgaben der Modellgestaltung, interaktiver Simulationen, numerischer Experimente und Datenanalyse in einer einzigen Entwicklungs-Umgebung. Das Paket ist für interaktives Programmieren mit IPython und Jupyter optimiert.
 
 Dokumentation: [agentpy.readthedocs.io](https://agentpy.readthedocs.io)			`,
@@ -126,48 +110,32 @@ Dokumentation: [agentpy.readthedocs.io](https://agentpy.readthedocs.io)			`,
 	];
 </script>
 
-<Modal />
-<!-- <Modal components={modalRegistry} /> -->
-
-<div class="h-full mx-auto flex justify-center items-center">
-	<div class="space-y-10 sm:space-y-20 max-w-5xl w-full p-10 md:p-24 lg:mt-35">
-		<div
-			id="pf-title"
-			class="flex flex-row gap-5 md:gap-10 items-center justify-between"
-		>
+<div class="mx-auto flex h-full items-center justify-center">
+	<div class="lg:mt-35 w-full max-w-5xl space-y-10 p-10 sm:space-y-20 md:p-24">
+		<div id="pf-title" class="flex flex-row items-center justify-between gap-5 md:gap-10">
 			<div class="w-2/3">
 				<h1 class="h1 mb-2">Joël Foramitti</h1>
-				<p class="text-xl md:text-2xl italic mb-4">
-					Ökologischer Ökonom & Software Entwickler
-				</p>
-				<div class="flex flex-wrap justify-left text-3xl space-x-4">
+				<p class="mb-4 text-xl italic md:text-2xl">Ökologischer Ökonom & Software Entwickler</p>
+				<div class="justify-left flex flex-wrap space-x-4 text-3xl">
 					{#each icons as { name, icon, href }}
-						<a {href} target="_blank"
-							><Icon {icon} class="hover:scale-[1.04] duration-100" /></a
-						>
+						<a {href} target="_blank"><Icon {icon} class="duration-100 hover:scale-[1.04]" /></a>
 					{/each}
 				</div>
 			</div>
 			<div class="drop-shadow-md">
-				<img
-					src="images/joel.jpg"
-					alt="Joël Foramitti"
-					class="rounded-full w-40 overflow-hidden"
-				/>
+				<img src="images/joel.jpg" alt="Joël Foramitti" class="w-40 overflow-hidden rounded-full" />
 			</div>
 		</div>
 
 		<div id="pf-intro" class="space-y-4">
 			<p class="text-lg">
-				Hallo! Ich forsche zu alternativen Wirtschaftsformen und unterstütze
-				Organisationen bei der Entwicklung von digitalen Lösungen für
-				Partizipationsprozesse und Selbstorganisation. Mein Schwerpunkt liegt
-				dabei auf Projekten für <a
+				Hallo! Ich forsche zu alternativen Wirtschaftsformen und unterstütze Organisationen bei der
+				Entwicklung von digitalen Lösungen für Partizipationsprozesse und Selbstorganisation. Mein
+				Schwerpunkt liegt dabei auf Projekten für <a
 					href="https://se-conference.uni-graz.at/de/politik/oesterreich/#c541464"
 					target="_blank"
 					class="font-bold italic"
-					>gemeinschaftliches, ökologisches und bedürfnis-orientiertes
-					Wirtschaften</a
+					>gemeinschaftliches, ökologisches und bedürfnis-orientiertes Wirtschaften</a
 				>.
 			</p>
 		</div>
@@ -176,28 +144,28 @@ Dokumentation: [agentpy.readthedocs.io](https://agentpy.readthedocs.io)			`,
 		<div id="pf-services" class="space-y-6">
 			<h2 class="h2">Angebote</h2>
 
-			<div class="grid grid-cols-1 sm:grid-cols-2 gap-5">
-				<div class="pf-box w-full px-6 py-5 space-y-2">
+			<div class="grid grid-cols-1 gap-5 sm:grid-cols-2">
+				<div class="pf-box w-full space-y-2 px-6 py-5">
 					<h3 class="h3">Software-Entwicklung</h3>
 					<p class="text-md">
-						Ich biete maßgeschneiderte Lösungen für Datenbanken, Apps und
-						digitale Plattformen mit einem Fokus auf Open-Source Technologien.
+						Ich biete maßgeschneiderte Lösungen für Datenbanken, Apps und digitale Plattformen mit
+						einem Fokus auf Open-Source Technologien.
 					</p>
 					<p class="text-md">
-						Dabei begleite ich den gesamten Entwicklungs-Prozess von der
-						gemeinsamen Konzeption bis zur Umsetzung und langfristigen Wartung.
+						Dabei begleite ich den gesamten Entwicklungs-Prozess von der gemeinsamen Konzeption bis
+						zur Umsetzung und langfristigen Wartung.
 					</p>
 				</div>
 
-				<div class="pf-box w-full px-6 py-5 space-y-2">
+				<div class="pf-box w-full space-y-2 px-6 py-5">
 					<h3 class="h3">Forschung & Beratung</h3>
 					<p>
-						Ich biete Unterstützung bei der Entwicklung von Strukturen und
-						Prozessen und für Partizipation und Selbstorganisation.
+						Ich biete Unterstützung bei der Entwicklung von Strukturen und Prozessen und für
+						Partizipation und Selbstorganisation.
 					</p>
 					<p>
-						Darüber hinaus biete ich Forschungsleistungen wie Workshops,
-						Umfragen, Datenerhebungen, Analysen und Simulationen an.
+						Darüber hinaus biete ich Forschungsleistungen wie Workshops, Umfragen, Datenerhebungen,
+						Analysen und Simulationen an.
 					</p>
 				</div>
 			</div>
@@ -207,14 +175,11 @@ Dokumentation: [agentpy.readthedocs.io](https://agentpy.readthedocs.io)			`,
 		<div id="projects" class="space-y-6">
 			<h2 class="h2">Projekte</h2>
 
-			<div
-				id="projects"
-				class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5"
-			>
+			<div id="projects" class="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
 				{#each projects as project}
 					<button on:click={() => openProjectModal(project)}>
-						<div class="pf-box h-full px-6 py-5 space-y-2">
-							<div class="flex flex-col space-y-2 m-1">
+						<div class="pf-box h-full space-y-2 px-6 py-5">
+							<div class="m-1 flex flex-col space-y-2">
 								<h3 class="h3">{project.title}</h3>
 								<p class="text-sm italic">{project.date}</p>
 								<p>{project.summary}</p>
@@ -228,15 +193,11 @@ Dokumentation: [agentpy.readthedocs.io](https://agentpy.readthedocs.io)			`,
 		<!-- Partners block -->
 		<div id="partners" class="space-y-6">
 			<h2 class="h2">Partner*innen</h2>
-			<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+			<div class="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
 				{#each partners as { name, href, logo }}
 					<a {href} target="_blank">
-						<div class="pf-box h-36 flex items-center">
-							<img
-								src={logo}
-								alt={name}
-								class="m-auto max-h-[7rem] max-w-[10rem]"
-							/>
+						<div class="pf-box flex h-36 items-center">
+							<img src={logo} alt={name} class="m-auto max-h-[7rem] max-w-[10rem]" />
 						</div>
 					</a>
 				{/each}
@@ -246,42 +207,41 @@ Dokumentation: [agentpy.readthedocs.io](https://agentpy.readthedocs.io)			`,
 		<!-- Background block -->
 		<div id="pf-cv" class="space-y-6">
 			<h2 class="h2">Hintergrund</h2>
-			<ul class="list-inside list-disc text-md pf-box p-5">
+			<ul class="text-md pf-box list-inside list-disc p-5">
 				<li>
-					<span class="font-bold">02.2024 - heute</span>: Selbstständig als
-					Software-Entwickler & Ökologischer Ökonom
+					<span class="font-bold">02.2024 - heute</span>: Selbstständig als Software-Entwickler &
+					Ökologischer Ökonom
 				</li>
 				<li>
-					<span class="font-bold">09.2022 - heute</span>: Operatives Team, MILA
-					Mitmach-Supermarkt e. G.
+					<span class="font-bold">09.2022 - heute</span>: Operatives Team, MILA Mitmach-Supermarkt
+					e. G.
 				</li>
 				<li>
-					<span class="font-bold">09.2018 - 09.2022</span>: Ph.D. Ökologische
-					Ökonomik, Vrije Universiteit Amsterdam
+					<span class="font-bold">09.2018 - 09.2022</span>: Ph.D. Ökologische Ökonomik, Vrije
+					Universiteit Amsterdam
 				</li>
 				<li>
-					<span class="font-bold">09.2017 - 05.2018</span>: M.Sc.
-					Umweltwissenschaften, Universitat Autònoma de Barcelona
+					<span class="font-bold">09.2017 - 05.2018</span>: M.Sc. Umweltwissenschaften, Universitat
+					Autònoma de Barcelona
 				</li>
 				<li>
-					<span class="font-bold">10.2013 - 01.2017</span>: B.Sc. Technische
-					Physik, Technische Universität Wien
+					<span class="font-bold">10.2013 - 01.2017</span>: B.Sc. Technische Physik, Technische
+					Universität Wien
 				</li>
 			</ul>
 		</div>
 
 		<div>
 			<a href="mailto:info@foramitti.org">
-				<button
-					type="button"
-					class="btn font-bold text-lg md:text-2xl rounded-md bg-primary-900 drop-shadow-md w-full px-6 py-5 text-white center"
+				<div
+					class="btn center flex w-full items-center justify-center gap-2 rounded-md bg-primary px-6 py-5 text-lg font-bold text-white drop-shadow-md transition hover:scale-[1.02] md:text-2xl"
 				>
 					<Icon icon="bi:envelope-at-fill" class="mr-2" /> Jetzt Kontakt aufnehmen
-				</button>
+				</div>
 			</a>
 		</div>
 
-		<div class="pf-box px-6 py-5 text-lg flex flex-wrap items-end">
+		<div class="pf-box flex flex-wrap items-end px-6 py-5 text-lg">
 			<div class="w-full sm:w-1/2">
 				<span class="font-semibold">Joël Foramitti, Ph.D.</span>
 				{#each icons as { label, icon, href }}
@@ -293,38 +253,20 @@ Dokumentation: [agentpy.readthedocs.io](https://agentpy.readthedocs.io)			`,
 			</div>
 			<div class="grow"></div>
 			<div class="flex flex-col items-start sm:items-end">
-				<button
-					class="link-on-white"
-					on:click={() =>
-						modalStore.trigger({
-							type: 'component',
-							component: { ref: Impressum }
-						})}
-				>
-					Impressum
-				</button>
-				<button
-					class="link-on-white"
-					on:click={() =>
-						modalStore.trigger({
-							type: 'component',
-							component: { ref: Privacy }
-						})}
-				>
-					Datenschutz
-				</button>
+				<!-- on:click={} -->
+				<button class="link-on-white"> Impressum </button>
+				<button class="link-on-white"> Datenschutz </button>
 			</div>
 		</div>
 	</div>
 </div>
 
-<style lang="postcss">
+<style>
 	.pf-box {
-		@apply bg-white/60 hover:bg-white duration-150 drop-shadow-sm rounded-md text-black text-left;
+		@apply rounded-md bg-white/60 text-left text-black drop-shadow-sm duration-150 hover:bg-white;
 	}
 
 	.link-on-white {
-		@apply no-underline font-semibold text-primary-900;
-		color: --theme-font-color-base;
+		@apply font-semibold no-underline;
 	}
 </style>
